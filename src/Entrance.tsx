9f8772b5d3f42f6e301796e125e2f22dcaa862f8
yorikhu <13003682137@163.com>
2022-07-26 18:13:00 +0800

    feat: init

---
+++ src/Entrance.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Login from './Login';
+
+type Props = {};
+
+const Entrance: React.FC<Props> = (props) => {
+  return (
+    <>
+      <Login />
+    </>
+  );
+};
+
+export default Entrance;
---
+++ src/Login/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+type Props = {};
+
+const Login: React.FC<Props> = (props) => {
+  return (
+    <div className='login'>
+      <h1>Login World!</h1>
+    </div>
+  );
+};
+
+export default Login;
